cdfc880143462e63028aa841abc72b66c15678dd
SM_Park <39216546+sm-amoled@users.noreply.github.com>
2021-11-21 23:10:05 +0900

    211121 ImPoster 1차 구현 완료
    구현 필요
    - user가 변환을 원하는 이미지 또는 원하는 색상 팔레트를 선택하는 기능
    - 위 사항 구현시 colorImage 함수와 이미지 창 띄우는 기능 분리
    - 다양한 색상팔레트 추가 필요함!
    
    코멘트
    - softenImage 함수에 cv2.filter2D 함수를 사용했는데, 5x5 blurring filter 같은걸 직접 구현하는 편이 좋을까요?
    
    구현 완료
    - 구현사항 step 1 ~ step 4
    - histogram 누적 기반의 영역 처리 (input에 따라 동적으로 구간 나누기)

---
+++ ImPoster/funct.py
@@ -0,0 +1,197 @@
+import numpy as np
+import cv2
+
+def image2gray(src):
+    # 이미지를 grayscale로 바꾸는 함수
+    height, width, color = src.shape
+    dst = src.copy()
+
+    for y in range(0, height):
+        for x in range(0, width):
+            grayColor = (src.item(y, x,0)+src.item(y, x,1)+src.item(y, x,2))/3
+            dst[y, x] = round(grayColor)
+
+    return dst
+
+def setBoundaryByHistogram(src):
+    # 각 Region 의 경계값을 히스토그램의 누적을 이용하여, 각 영역의 픽셀 개수가 총 개수의 1/6이 되도록 정함 
+    height, width, color = src.shape
+
+    pixelNumOfEachBoundary = height * width / 6
+
+    stack = [0 for i in range(256)]
+    maxValueOfEachBoundary = []
+
+    for y in range(0, height):
+        for x in range(0, width):
+            stack[src.item(y, x, 0)] += 1
+
+    countNumb = 0
+    for i in range(0, 255):
+        countNumb += stack[i]
+        if countNumb >= pixelNumOfEachBoundary :
+            countNumb -= pixelNumOfEachBoundary
+            maxValueOfEachBoundary.append(i)
+            print(i)
+
+    return maxValueOfEachBoundary
+
+
+
+def devideLevel(src):
+    # setBoundaryByHistogram 함수를 이용하여 나눈 영역값을 기준으로 6단계(0~5)로 나누어주기
+    height, width, color = src.shape
+    
+    maxValueOfEachBoundary = setBoundaryByHistogram(src)
+
+    dst = src.copy();
+
+    for y in range(0, height):
+        for x in range(0, width):
+            if src.item(y, x, 0) < maxValueOfEachBoundary[0]:
+                dst[y, x] = 0
+
+            elif src.item(y, x, 0) < maxValueOfEachBoundary[1]:
+                dst[y, x] = 1
+
+            elif src.item(y, x,0) < maxValueOfEachBoundary[2]:
+                dst[y, x] = 2
+
+            elif src.item(y, x, 0) < maxValueOfEachBoundary[3]:
+                dst[y, x] = 3
+
+            elif src.item(y, x, 0) < maxValueOfEachBoundary[4]:
+                dst[y, x] = 4
+
+            else :
+                dst[y, x] = 5
+
+    
+    return dst
+
+def softenImage(src):
+    # 0~5로 나눠진 그림에서 5x5 평균값 필터로 이미지 노이즈를 제거 + 영역의 경계선 부드럽게 만들어주기
+    height, width, color = src.shape
+    dst = src.copy()
+
+    mask55 = np.ones((5,5), np.float64) / 25
+    cv2.filter2D(src, -1, mask55, dst)
+
+    return dst
+    
+def visualizeImage(src):
+    # 위 devide level 함수 결과 디버그 용도
+    # 0~5단계로 나뉜 영역을 가시화하여 grayscale img로 보여준다.
+
+    height, width, color = src.shape
+    dst = src.copy()
+
+    for y in range(0, height):
+        for x in range(0, width):
+            dst[y, x] = src[y, x] * (255 / 6) 
+
+    return dst
+
+# 테두리를 출력 시도해보다가 포기함!
+# 추후에 안쓰면 지우면 됨!
+# def drawEdge(src):
+#     #input : 0~7로 labeling 된 이미지
+    
+#     height, width, color = src.shape
+
+#     dst = src.copy()
+#     distance = 3;
+
+#     maskY = [distance, distance, distance, 0]
+#     maskX = [-distance, 0, distance, distance]
+    
+#     for y in range(0, height):
+#         for x in range(0, width):
+#             for i in range(0, 4):
+#                 y_ = y + maskY[i]
+#                 x_ = x + maskX[i]
+
+#                 if (y_ < height) and ( x_ >= 0 and x_ < width) :
+#                     if src.item(y, x, 0) - src.item(y_, x_, 0) >= 3:
+#                         for j in range(0, round(distance / 1)) :
+#                             dst[round(y + maskY[i]/distance * j), round(x + maskX[i]/distance * j)] = 0
+
+#                     if src.item(y, x, 0) - src.item(y_, x_, 0) <= -2:
+#                         for j in range(0, round(distance / 1)) :
+#                             dst[round(y + maskY[i]/distance * j), round(x + maskX[i]/distance * j)] = 0
+                    
+#                 else :
+#                     continue
+    
+#     return dst
+
+
+def colorImage(src):
+    #팔레트를 이용해 영역에 색칠하기
+    #파격적인 색상조합을 추가해주기 바랍니당
+    height, width, color = src.shape
+
+    # **못생긴 팔레트라 변경 필요함!**
+    palette1 = [
+        (0, 0, 0), #검은색
+        (77, 50, 0), #남색
+        (163, 153, 118), #푸른색
+        (37,  23, 224),#빨간색
+        (169, 229, 253), #아이보리
+        (210,  210, 238) #흰색
+    ]
+
+    palette2 = [
+        (0, 0, 0), #검은색
+        (164, 46, 105), #보라색
+        (13, 100, 210), #귤색
+        (50, 170, 240), #덜연한귤색
+        (164,  227, 255),#연한귤색
+        (242, 250, 253) #흰색
+    ]
+
+    palette3 = [
+        (0, 0, 0), #블랙
+        (40, 41, 38), #검은색
+        (125, 99, 22), #심해색
+        (44, 189, 250), #하늘색
+        (251, 194, 156), #살구색
+        (255,  248, 178) #연노랑
+    ]
+
+    palette4 = [
+        (148, 58, 108), #보라
+        (58, 19, 189), #목젖색
+        (233,  151, 35), #뽕따꼭따리
+        (60, 200, 255), #레몬껍질색
+        (141, 243, 197), #메로나
+        (208, 246, 250) #귤피색
+        #여기에 팔레트 색상 추가
+    ]
+
+    # 못생긴 팔레트라 변경 필요함!
+    # dst = src.copy()
+    # for y in range(0, height):
+    #     for x in range(0, width):
+    #         dst[y, x] = palette1[src.item(y, x, 0)]
+    # cv2.imshow("1", dst)
+
+    dst = src.copy()
+    for y in range(0, height):
+        for x in range(0, width):
+            dst[y, x] = palette2[src.item(y, x, 0)]
+    cv2.imshow("2", dst)
+
+    dst = src.copy()
+    for y in range(0, height):
+        for x in range(0, width):
+            dst[y, x] = palette3[src.item(y, x, 0)]
+    cv2.imshow("3", dst)
+
+    dst = src.copy()
+    for y in range(0, height):
+        for x in range(0, width):
+            dst[y, x] = palette4[src.item(y, x, 0)]
+    cv2.imshow("4", dst)
+
+    return dst
---
+++ ImPoster/process.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import funct
+
+# 여기에서 파일명을 바꾸며 출력해주고 있음!
+img = cv2.imread("./image/shirtman.png")
+img = funct.image2gray(img)
+img = funct.devideLevel(img)
+img = funct.softenImage(img)
+
+# 어떤 color palette의 이미지를 출력할지 선택하는 기능 추가 필요
+funct.colorImage(img)
+
+# 현재 컬러 디버깅을 위해 여러 팔레트를 한 번에 출력하고자 함수에 imshow를 넣어뒀는데, 
+# 기능 추가시 / 프로그램 제출 전에 imshow 함수를 밖으로 빼내는게 좋을 것 같네요
+# img = funct.colorImage(img)
+# cv2.imshow("result", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
